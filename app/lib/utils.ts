f95da2aa175a153c0c6179d2f5b225d2f56daee4
aarondesu <aaronneielaro@gmail.com>
2026-02-12 15:29:54 +0800

    feat: add shadcn components

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,6 +1,11 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function randString() {
+  let random = (Math.random() + 1).toString(36).substring(7);
+  return random;
 }
