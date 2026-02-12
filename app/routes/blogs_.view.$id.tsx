c206dec80a22991f6679e0f27591b543affabd25
aarondesu <aaronneielaro@gmail.com>
2026-02-12 15:32:15 +0800

    feat: add image to blog

--- app/routes/blogs_.view.$id.tsx
+++ app/routes/blogs_.view.$id.tsx
@@ -9,8 +9,8 @@ import dayjs from "dayjs";
 export async function loader({ request, params }: Route.LoaderArgs) {
   const client = getSupabaseServerClient(request);
   const result = await client
-    .from("blogs")
-    .select()
+    .from("view_blog_with_username")
+    .select("*")
     .eq("id", Number(params.id))
     .range(0, 1);
 
@@ -44,15 +44,26 @@ export default function ViewBlog({ loaderData }: Route.ComponentProps) {
   const { blog } = loaderData;
 
   return (
-    <div className="container mx-auto space-y-4 my-4">
-      <div className="">
-        <h1 className="text-3xl font-black">{blog?.title}</h1>
-        <p className="font-medium text-muted-foreground text-sm">
-          {dayjs(blog?.created_at).format("MMM DD, YYYY HH:mm:ss")}
-        </p>
+    <div className="container mx-auto space-y-6 my-4">
+      <div className="space-y-4">
+        <div className="">
+          <h1 className="text-3xl font-black">{blog?.title}</h1>
+          <span className="flex gap-2">
+            <p className="font-medium text-muted-foreground text-sm">
+              {dayjs(blog?.created_at).format("MMM DD, YYYY HH:mm:ss")}
+            </p>
+            <p className="text-sm text-muted-foreground font-bold">
+              by {blog?.author}
+            </p>
+          </span>
+        </div>
+        {blog?.image_url && <img src={blog.image_url} />}
+        <div>
+          <p className="whitespace-pre-wrap">{blog?.body}</p>
+        </div>
       </div>
-      <div>
-        <p className="whitespace-pre-wrap">{blog?.body}</p>
+      <div className="border-t pt-3">
+        <h4 className="font-bold text-xl">Comments (0)</h4>
       </div>
     </div>
   );
