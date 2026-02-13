aabf9ce66eafbfadd0701393cef118f8194bcd98
aarondesu <aaronneielaro@gmail.com>
2026-02-13 16:24:13 +0800

    refactor: dialog rewrite

--- app/components/confirm-delete-blog-dialog.tsx
+++ app/components/confirm-delete-blog-dialog.tsx
@@ -11,6 +11,14 @@ import { useCallback, useState } from "react";
 import { Button } from "./ui/button";
 import { Form, useNavigation, useSubmit } from "react-router";
 import { Loader2Icon } from "lucide-react";
+import { useIsMobile } from "~/hooks/use-mobile";
+import {
+  Drawer,
+  DrawerContent,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "./ui/drawer";
 
 type Args = {
   children: React.ReactNode;
@@ -18,7 +26,7 @@ type Args = {
   title: string;
 };
 
-export default function ConfirmDeleteBlogDialog({ children, id, title }: Args) {
+function DeleteDialogForm({ id, title }: { id: number; title: string }) {
   const [check, setCheck] = useState<string>("");
   const navigation = useNavigation();
 
@@ -31,44 +39,59 @@ export default function ConfirmDeleteBlogDialog({ children, id, title }: Args) {
   }, []);
 
   return (
-    <Dialog>
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        onSubmit();
+      }}
+      className="space-y-4"
+    >
+      <div className="text-sm space-y-2">
+        <p>This will permanently delete this blog and it's comments.</p>
+        <p className="">
+          Type <b>{title}</b> to confirm.
+        </p>
+
+        <Input
+          type="text"
+          value={check}
+          onChange={(e) => setCheck(e.target.value)}
+        />
+      </div>
+      <div className="flex">
+        <Button
+          className="w-full"
+          disabled={check !== title || navigation.state !== "idle"}
+          type="submit"
+        >
+          {navigation.state !== "idle" && (
+            <Loader2Icon className="animate-spin" />
+          )}
+          Confirm
+        </Button>
+      </div>
+    </form>
+  );
+}
+
+/**
+ * Confirmation dialog
+ * TODO: Add mobile dialog
+ * @param param0
+ * @returns
+ */
+export default function ConfirmDeleteBlogDialog({ children, id, title }: Args) {
+  const isMobile = useIsMobile();
+  const [open, setOpen] = useState<boolean>(false);
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
       <DialogTrigger asChild>{children}</DialogTrigger>
-      <DialogContent>
+      <DialogContent className="data-[vaul-drawer-direction=bottom]:min-h-[50vh] data-[vaul-drawer-direction=top]:min-h-[50vh]">
         <DialogHeader>
           <DialogTitle>Delete blog</DialogTitle>
         </DialogHeader>
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            onSubmit();
-          }}
-          className="space-y-4"
-        >
-          <div className="text-sm space-y-2">
-            <p>This will permanently delete this blog and it's comments.</p>
-            <p className="">
-              Type <b>{title}</b> to confirm.
-            </p>
-
-            <Input
-              type="text"
-              value={check}
-              onChange={(e) => setCheck(e.target.value)}
-            />
-          </div>
-          <div className="flex">
-            <Button
-              className="w-full"
-              disabled={check !== title || navigation.state !== "idle"}
-              type="submit"
-            >
-              {navigation.state !== "idle" && (
-                <Loader2Icon className="animate-spin" />
-              )}
-              Confirm
-            </Button>
-          </div>
-        </form>
+        <DeleteDialogForm id={id} title={title} />
       </DialogContent>
     </Dialog>
   );
